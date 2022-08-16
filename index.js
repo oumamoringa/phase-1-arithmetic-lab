992d2daa66d66890a2e88d69600d2ca413c8cd37
oumamoringa <richard.ouma@student.moringaschool>
2022-08-16 19:57:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random()+1);
+console.log(random);
+
+let num3 = 13;
+let num4 = 9;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = (14, 20, 16, 20);
+console.log(Math.max(max));
